690f8007c9ac0a1a604d9907e12f15ab32ea6d9a
spaenleh <spaenleh@gmail.com>
2024-01-04 17:50:16 +0100

    add misc section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import About from '../sections/About.svelte';
 	import Home from '../sections/Home.svelte';
 	import Projects from '../sections/Projects.svelte';
+	import DetailedExperience from '../sections/Misc.svelte';
 </script>
 
 <Home />
@@ -9,3 +10,5 @@
 <About />
 
 <Projects />
+
+<DetailedExperience />
---
+++ src/sections/Misc.svelte
@@ -0,0 +1,26 @@
+<script>
+	import HeaderSpacer from '../components/HeaderSpacer.svelte';
+</script>
+
+<div id="detailed-experience" class="flex flex-col mb-10">
+	<HeaderSpacer />
+	<h2 class="text-2xl font-bold">Miscellaneous</h2>
+	<div class="flex flex-col gap-2 my-2">
+		<h3 class="font-bold text-lg">Dev Ops</h3>
+		<p>
+			<strong> Github Actions automation:</strong> I have experience using github actions to perform
+			automated testing, builds and deployments
+		</p>
+		<p>
+			<strong>AWS infrastructure:</strong> Working with a full-fledged app hosted on AWS infra, I have
+			experience navigating logs, container and services configurations, S3 storage, cloudfront CDN,
+			databases, load balancers and hosted zones (DNS).
+		</p>
+		<p>
+			<strong>Clean Code:</strong> I value clean code and have been doing code reviews for various projects.
+			I enjoy contributing to Open Source projects when I can. Without getting too dogmatical I like
+			to write idiomatic code that can be easily understood. I know when to do one-liners and when to
+			write boring code that can be more easily changed and maintained in the future.
+		</p>
+	</div>
+</div>
